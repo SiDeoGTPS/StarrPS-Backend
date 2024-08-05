089ceab58eaab9ebdddcc8aad3041d3cdfba92e3
StarrGTPS <159306176+SiDeoGTPS@users.noreply.github.com>
2024-08-06 02:31:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -46,6 +46,6 @@ app.get('/', function (req, res) {
     res.send('SpyTopia Here, Hello World!');
 });
 
-app.listen(444, function () {
-    console.log('Listening on port 444');
+app.listen(5000, function () {
+    console.log('Listening on port 5000');
 });

10504d52861fbae81e17156000f9cf4b5f8068fd
StarrGTPS <159306176+SiDeoGTPS@users.noreply.github.com>
2024-08-06 02:43:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,43 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const path = require('path');
 
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept',
+    );
     next();
 });
-
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.json());
 
-// Middleware to log the request method and URL
 app.use(function (req, res, next) {
     console.log(req.method, req.url);
     next();
 });
 
-// Serve the dashboard directly when accessing the root
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/html/dashboard.html'));
+app.use(express.json());
+
+app.post('/player/login/dashboard', function (req, res) {
+    // redirect to path /player/growid/login/validate
+    res.redirect('/player/growid/login/validate');
 });
 
-// Respond to login attempts by closing the window
 app.post('/player/growid/login/validate', (req, res) => {
-    res.send('<script>window.close();</script>');
+    res.send(
+        `{"status":"success","message":"Account Validated.","token":"BE SUPER DAY PLEASE","url":"","accountType":"growtopia"}`,
+    );
 });
 
-// Close the window script
 app.post('/player/validate/close', function (req, res) {
     res.send('<script>window.close();</script>');
 });
 
-// Start the server
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
 app.listen(5000, function () {
     console.log('Listening on port 5000');
 });
